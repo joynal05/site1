8c560822f7f2eb65db0e0ed8cd04c4bd7a2d891e
joynal9933 <joynal.9933@gmail.com>
2022-02-12 22:18:44 +0600

    before marge

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,38 @@
+import { Link } from "react-router-dom";
+import './Header.css'
+
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>
-        <ul>
-            <li>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-            </li>
-        </ul>
-
-
-    </div>
+    <header className="site-header">
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <div className="site-info">
+                        <Link to="/">
+                            <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png" alt="" />
+                        </Link>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="header-menu flex-right">
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/about">About</Link>
+                            </li>
+                            <li>
+                                <Link to="/portfolio">Portfolio</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
   )
 }
 
